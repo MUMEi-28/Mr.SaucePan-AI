30cfb6a733709af7b8c5ae7053ba7e46566e6e14
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-01-13 14:44:25 +0800

    Updates

--- src/components/GetRecipe.jsx
+++
@@ -1,9 +0,0 @@
-export default function GetRecipe(props)
-{
-    return (
-        <section className="get-recipe">
-            <h1>Ready for A Recipe?</h1>
-            <button onClick={props.showRecipe}>Generate</button>
-        </section>
-    )
-}
